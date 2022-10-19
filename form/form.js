bb954644b306154940e278439bfb1cd339785bf3
Tyler <111825114+WeezerFan096@users.noreply.github.com>
2022-10-18 18:15:17 -0700

    comments
    made commments

--- form/form.js
+++ form/form.js
@@ -2,3 +2,4 @@ function eyeball(){
     alert("Thank you for ordering from us!");
    console.log("There are no more barriers to cross. All I have in common with the uncontrollable and the insane, the vicious and the evil, all the mayhem I have caused and my utter indifference toward it I have now surpassed. My pain is constant and sharp and I do not hope for a better world for anyone. In fact, I want my pain to be inflicted on others. I want no one to escape, but even after admitting this, there is no catharsis. My punishment continues to elude me and I gain no deeper knowledge of myself. No new knowledge can be extracted from my telling. This confession has meant nothing.")
 }
+// OH MY GOD WHEN WILL IT END
